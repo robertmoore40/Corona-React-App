59bec812bb802bc0c69c119c7e4cef7ede093cec
robertmoore40 <robertcmoore92@gmail.com>
2020-05-04 22:38:20 -0400

    added line chart, other touches

--- src/App.js
+++ src/App.js
@@ -1,36 +1,39 @@
 import React from 'react';
+import { Cards, CountryPicker, Chart } from './components';
 
-import { Cards, Chart, CountryPicker} from './components';
-import styles from './App.module.css'
-import { fetchData } from './api'
+import { fetchData } from './api/';
+import styles from './App.module.css';
 
-class App extends React.Component{
 
+class App extends React.Component {
     state = {
-        data: {},
-    
+      data: {},
+      country: '',
     }
-
+  
     async componentDidMount() {
-        const fetchedData = await fetchData();
-
-        this.setState({ data : fetchedData})
-
-        console.log(fetchedData);
+      const data = await fetchData();
+  
+      this.setState({ data });
     }
-
+  
+    handleCountryChange = async (country) => {
+      const data = await fetchData(country);
+  
+      this.setState({ data, country: country });
+    }
+  
     render() {
-
-        const { data } = this.state;
-
-        return (
-            <div className={styles.container}>
-               <Cards data={this.state.data} />
-               <CountryPicker />
-               <Chart />
-            </div>
-        )
+      const { data, country } = this.state;
+  
+      return (
+        <div className={styles.container}>
+          <Cards data={data} />
+          <CountryPicker handleCountryChange={this.handleCountryChange} />
+          <Chart data={data} country={country} /> 
+        </div>
+      );
     }
-}
-
-export default App
+  }
+  
+  export default App;
--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,24 +1,48 @@
-import React, { useState, useEffect } from "react";
-import { fetchDailyData } from "../../api";
-import { Line, Bar } from "react-chartjs-2";
-import styles from "./Chart.module.css";
+  
+import React, { useState, useEffect } from 'react';
+import { Line, Bar } from 'react-chartjs-2';
 
-const Chart = () => {
+import { fetchDailyData } from '../../api';
+
+import styles from './Chart.module.css';
+
+
+const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
   const [dailyData, setDailyData] = useState({});
 
   useEffect(() => {
     const fetchMyAPI = async () => {
-      setDailyData(await fetchDailyData());
-    };
+      const initialDailyData = await fetchDailyData();
 
-    console.log(dailyData);
+      setDailyData(initialDailyData);
+    };
 
     fetchMyAPI();
   }, []);
 
+  const barChart = (
+    confirmed ? (
+      <Bar
+        data={{
+          labels: ['Infected', 'Recovered', 'Deaths'],
+          datasets: [
+            {
+              label: 'People',
+              backgroundColor: ['rgba(0, 0, 255, 0.5)', 'rgba(0, 255, 0, 0.5)', 'rgba(255, 0, 0, 0.5)'],
+              data: [confirmed.value, recovered.value, deaths.value],
+            },
+          ],
+        }}
+        options={{
+          legend: { display: false },
+          title: { display: true, text: `Current state in ${country}` },
+        }}
+      />
+    ) : null
+  );
+
   const lineChart = (
-    dailyData.length
-  ? (
+    dailyData[0] ? (
       <Line
         data={{
           labels: dailyData.map(({ date }) => date),
@@ -38,11 +62,9 @@ const Chart = () => {
         }}
       />
     ) : null
-  );
-
-  return (
+  ); return (
     <div className={styles.container}>
-      {lineChart}
+      {country ? barChart : lineChart}
     </div>
   );
 };
